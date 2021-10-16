380fd1717498326512420e56b706abef3f1f5436
( <tgilbert@fastmail.com.au>
2021-10-16 17:34:03 +1100

    added draw method and clock for timing

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from pygame.event import Event
+from pygame.time import Clock
+from pygame.surface import Surface
+from pygame import Color
 
 pygame.init()
 
 
 class RaycastingGame:
-    def __init__(self):
+    def __init__(self, background_colour: Color = (255, 255, 255)):
+        self.background_colour = background_colour
+
+        self.clock = Clock()
         self.running = False
         self.key_map = {
             pygame.K_ESCAPE: self.quit
@@ -20,13 +26,22 @@ class RaycastingGame:
                 if event.key in self.key_map.keys():
                     self.key_map[event.key]()
 
+    def update(self):
+        delta_time = self.clock.tick()
+        self.process_events(pygame.event.get())
+
+    def draw(self, surface: Surface):
+        surface.fill(self.background_colour)
+
     def mainloop(self):
         window = pygame.display.set_mode((0, 0))
 
         self.running = True
 
         while self.running:
-            self.process_events(pygame.event.get())
+            self.update()
+            self.draw(window)
+            pygame.display.flip()
 
 
 if __name__ == '__main__':

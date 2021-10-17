d1f1f17537180f9951ac6414b6cf6be7ec5fa84c
( <tgilbert@fastmail.com.au>
2021-10-17 17:19:12 +1100

    added map

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.event import Event
 from pygame.time import Clock
 from pygame.surface import Surface
 from pygame import Color
+from game_map import MapHelper
 
 pygame.init()
 
@@ -16,6 +17,7 @@ class RaycastingGame:
         self.key_map = {
             pygame.K_ESCAPE: self.quit
         }
+        self.map = MapHelper.generate_random_map((16, 16))
 
     def quit(self):
         self.running = False
@@ -32,6 +34,28 @@ class RaycastingGame:
 
     def draw(self, surface: Surface):
         surface.fill(self.background_colour)
+        self.draw_map(surface)
+
+    def draw_map(self, surface: Surface):
+        cell_height = int(surface.get_height() / self.map.shape[0])
+        cell_width = cell_height
+
+        centre_offset_x = (surface.get_width() - cell_width * self.map.shape[0]) / 2
+        centre_offset_y = (surface.get_height() - cell_height * self.map.shape[1]) / 2
+
+        for row in range(self.map.shape[0]):
+            for col in range(self.map.shape[1]):
+                if self.map[row, col]:
+                    colour = (255, 0, 0)
+                else:
+                    colour = (0, 0, 0)
+                rect = (
+                    col * cell_width + centre_offset_x,
+                    row * cell_height + centre_offset_y,
+                    cell_width,
+                    cell_height
+                )
+                pygame.draw.rect(surface, colour, rect)
 
     def mainloop(self):
         window = pygame.display.set_mode((0, 0))

6ef23f40062de06c528d37454eec648958ef9540
( <tgilbert@fastmail.com.au>
2021-11-08 15:20:11 +1100

    made shift+f3 hide debug info and added rudimentary logging

--- debug.py
+++ debug.py
@@ -1,4 +1,5 @@
 from game import RaycastingGame
+import logging
 from typing import Callable
 import pygame
 from pygame.freetype import SysFont
@@ -8,6 +9,8 @@ pygame.init()
 
 
 PayloadMethod = Callable[[Callable, ...], None]
+
+debugger_logger = logging.getLogger("debugger")
 
 
 class Payload:
@@ -26,7 +29,8 @@ class Payload:
 
 class Debugger:
     @staticmethod
-    def standard_inject(payload: Payload, instance: object, target_name: str):
+    def standard_inject(payload: Payload, instance: object, target_name: str) -> Callable:
+        debugger_logger.debug(f"Standard injecting payload {payload} into method {target_name} of instance {instance}")
         target = getattr(instance, target_name)
 
         def injection(*args, **kwargs):
@@ -36,6 +40,8 @@ class Debugger:
             return result
         setattr(instance, target_name, injection)
 
+        return injection
+
 
 class RaycastingGameDebugger(Debugger):
     DEBUG_MOD_KEY = pygame.KMOD_LSHIFT
@@ -44,22 +50,30 @@ class RaycastingGameDebugger(Debugger):
         self.delta_time = 0
         self.font: SysFont = SysFont("", 30)
         self.instance: RaycastingGame = instance
+        self.show_debug_info: bool = True
 
     def toggle_noclip(self, event):
         if event.mod == RaycastingGameDebugger.DEBUG_MOD_KEY and event.type == pygame.KEYDOWN:
             self.instance.player.clip = not self.instance.player.clip
 
+    def toggle_debug_info(self, event):
+        if event.mod == RaycastingGameDebugger.DEBUG_MOD_KEY and event.type == pygame.KEYDOWN:
+            self.show_debug_info = not self.show_debug_info
+
     def update_payload_before(self, target: Callable, *args, **kwargs):
         self.delta_time = args[0]
 
     def draw_payload_after(self, target: Callable, *args, **kwargs):
+        if not self.show_debug_info:
+            return
+
         surface = args[0]
 
         lines = [
             str(1 / self.delta_time),
-            str(target.__self__.player.camera_plane),
-            str(target.__self__.player.forward),
-            str(target.__self__.player.position),
+            str(target.__self__.game.player.camera_plane),
+            str(target.__self__.game.player.forward),
+            str(target.__self__.game.player.position),
             "NOCLIP" if not self.instance.player.clip else ""
         ]
 
@@ -68,10 +82,11 @@ class RaycastingGameDebugger(Debugger):
 
     def inject(self, instance: RaycastingGame):
         RaycastingGameDebugger.standard_inject(Payload(self.update_payload_before), instance, "update")
-        RaycastingGameDebugger.standard_inject(Payload(after=self.draw_payload_after), instance, "draw")
+        instance.draw_mode_map[RaycastingGame.DrawMode.GAME] = RaycastingGameDebugger.standard_inject(Payload(after=self.draw_payload_after), instance.game_renderer, "draw")
 
     def init(self, instance: RaycastingGame):
         instance.key_map[pygame.K_n] = self.toggle_noclip
+        instance.key_map[pygame.K_F3] = self.toggle_debug_info
 
     def start(self):
         self.init(self.instance)
